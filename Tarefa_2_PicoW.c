267833516c95866490713dd18d770fe2689b3817
Luinix <luizmarcelolh@gmail.com>
2025-01-26 15:59:01 -0300

    Update Tarefa_2_PicoW.c

--- Tarefa_2_PicoW.c
+++ Tarefa_2_PicoW.c
@@ -674,7 +674,7 @@ void cobrinhaAnimation(PIO pio, uint sm)
                  (direcao == 1 && novaDirecao == 0) ||
                  (direcao == 2 && novaDirecao == 3) ||
                  (direcao == 3 && novaDirecao == 2)); // Evitar direção oposta direta
-        if (rand() % 100 < 40)
+        if (rand() % 100 < 35)
         {
             direcao = novaDirecao;
         }
